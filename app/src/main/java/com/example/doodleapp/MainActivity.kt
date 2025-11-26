fa5b0a8d89d7a97cd8dc3cb6eeb670e16071f7d1
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2025-11-26 09:16:26 -0500

    Add basic canvas drawing feature

--- app/src/main/java/com/example/doodleapp/MainActivity.kt
+++ app/src/main/java/com/example/doodleapp/MainActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.doodleapp.ui.theme.DoodleAppTheme
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DoodleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DrawingScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DrawingScreen() {
+    val paths = remember { mutableStateListOf<Path>() }
+    var forceRecompose by remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DoodleAppTheme {
-        Greeting("Android")
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(true) {
+                detectDragGestures(onDragStart = {
+                    paths.add(Path().apply { moveTo(it.x, it.y) })
+                }, onDrag = { change, _ ->
+                    change.consume()
+                    paths.last().lineTo(change.position.x, change.position.y)
+                    forceRecompose++
+                })
+            }) {
+        paths.forEach { path ->
+            drawPath(
+                path = path, color = Color.Black, style = Stroke(width = 5f)
+            )
+        }
     }
 }
